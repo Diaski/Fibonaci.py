02a3a328afe469810701944834d62d133d309346
Diaski <103786645+Diaski@users.noreply.github.com>
2022-05-21 17:12:04 +0200

    Add files via upload

---
+++ Fibonaci.py
@@ -0,0 +1,24 @@
+ans = int(input("which number of fibonaci you want to know? : "))
+a = 1
+b = 1
+i = 2
+if ans in [1,2]:
+    print(b)
+elif ans%2 == 0:
+    while i < ans:
+        a += b
+        b += a
+        i += 2
+    print(b)
+else:
+    while True:
+        a = a+b
+        i += 1
+        if (i == ans):
+            print( a)
+            break
+        b += a
+        i += 1
+        
+        
+    
